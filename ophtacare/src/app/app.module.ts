7ccd29e76220e76ab1e312c183ce2ce0f5d8540a
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-03-20 17:48:38 +0100

    set up angular material

--- ophtacare/src/app/app.module.ts
+++ ophtacare/src/app/app.module.ts
@@ -7,8 +7,33 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthenticationService } from './services/authenticationService';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { AuthGuard } from './guard/auth.guard';
+import {ButtonModule} from 'primeng/button';
+import {
+  MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatTableModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatChipsModule,
+    MatCheckboxModule, MatPaginatorModule,
+} from '@angular/material';
 
 const appRoutes: Routes = [
   {
@@ -28,6 +53,29 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatTableModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatChipsModule,
+    MatCheckboxModule, MatPaginatorModule,
+    FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, { useHash: true })
