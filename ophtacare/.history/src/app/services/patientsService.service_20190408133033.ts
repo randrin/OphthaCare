e1c5b2455bc8ec1e23d6b7fad7fcf2a97fef47e2
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-04-08 22:14:00 +0200

    Rest api insert Patient

---
+++ ophtacare/.history/src/app/services/patientsService.service_20190408133033.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http, ResponseContentType, Headers } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientsServiceService {
+
+  private patientExportUrl = window['baseUrl'] + '/excel/downloadExcelPatients';
+  private patientsUrl = window['baseUrl'] + '/patient/getAll';
+  private patientUrlInsert = window['baseUrl'] + '/patient/insert';
+  private patientUrlDelete = window['baseUrl'] + '/patient/delete';
+
+    constructor(
+        public router: Router, private http: Http) {
+    }
+
+    getAllPatients (caller) {
+      const options = {
+        headers: new Headers({
+          'caller': caller
+        }),
+    };
+    return this.http.get(this.patientsUrl);
+    }
+
+    insertPatient (caller) {
+      const options = {
+        headers: new Headers({
+          'caller': caller
+        }),
+    };
+    return this.http.post(this.patientUrlInsert, options);
+    }
+
+    deletePatient (patient, caller) {
+      const options = {
+        headers: new Headers({
+          'caller': caller
+        }),
+    };
+    return this.http.delete(this.patientUrlDelete + '/' + patient.idPatient, options);
+    }
+
+    exportPatients(caller) {
+      const options = {
+        headers: new Headers({
+          'caller': caller
+        }),
+        responseType: ResponseContentType.Blob
+    };
+    return this.http.get(this.patientExportUrl, options);
+    }
+}
