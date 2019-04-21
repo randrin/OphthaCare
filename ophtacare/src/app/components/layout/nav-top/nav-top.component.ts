eaa658851d1fcf8d65a54ae441127d9754274643
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-04-21 23:22:42 +0200

    Current Time Implementation

--- ophtacare/src/app/components/layout/nav-top/nav-top.component.ts
+++ ophtacare/src/app/components/layout/nav-top/nav-top.component.ts
@@ -9,6 +9,14 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class NavTopComponent implements OnInit {
   public pushRightClass: string;
+  public monthNames;
+  public dayNames;
+  public newDate;
+  public Date;
+  public Seconds;
+  public Hours;
+  public Minutes;
+
   constructor(private translate: TranslateService, private router: Router) {
     this.router.events.subscribe(val => {
       if (val instanceof NavigationEnd && window.innerWidth <= 992 && this.isToggled()) {
@@ -19,6 +27,47 @@ export class NavTopComponent implements OnInit {
 
   ngOnInit() {
     this.pushRightClass = 'push-right';
+    this.monthNames = [ 'January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    this.dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    this.getCurrentTime();
+  }
+
+  getCurrentTime() {
+    // Make single object
+    this.newDate = new Date();
+      
+		// Make current time
+    this.newDate.setDate(this.newDate.getDate());
+    
+    // Setting date and time
+    this.Date = this.dayNames[this.newDate.getDay()] + ', ' + this.newDate.getDate() + ' '
+    + this.monthNames[this.newDate.getMonth()] + ' ' + this.newDate.getFullYear();
+
+    setInterval( function() {
+      // Create a newDate() object and extract the seconds of the current time on the visitor's
+      const seconds = new Date().getSeconds();
+
+      // Add a leading zero to seconds value
+      this.Seconds = (seconds < 10 ? '0' : '' ) + seconds;
+    }, 1000);
+
+    setInterval( function() {
+      // Create a newDate() object and extract the minutes of the current time on the visitor's
+      const minutes = new Date().getMinutes();
+
+      // Add a leading zero to minutes value
+      this.Minutes = (minutes < 10 ? '0' : '' ) + minutes;
+    }, 1000);
+
+    setInterval( function() {
+      // Create a newDate() object and extract the hours of the current time on the visitor's
+      const hours = new Date().getHours();
+
+      // Add a leading zero to hours value
+      this.Hours = (hours < 10 ? '0' : '' ) + hours;
+    }, 1000);
   }
 
   isToggled(): boolean {
