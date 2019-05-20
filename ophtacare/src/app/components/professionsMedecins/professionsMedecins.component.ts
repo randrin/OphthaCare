d64e7e9585d8dc7b9db081f20f4758d7ade5a72d
Randrin <nzeukang.randrin@accenture.com>
2019-05-20 18:48:42 +0200

    Export Professions Medecins

--- ophtacare/src/app/components/professionsMedecins/professionsMedecins.component.ts
+++ ophtacare/src/app/components/professionsMedecins/professionsMedecins.component.ts
@@ -5,6 +5,8 @@ import { MessageService } from 'primeng/api';
 import { ConfirmationService } from 'primeng/primeng';
 import { Profession } from '../../models/professions/profession';
 import { Professions } from '../../models/professions/professions';
+import saveAs from 'save-as';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-professionsmedecins',
@@ -23,6 +25,10 @@ export class ProfessionsMedecinsComponent implements OnInit {
   public profession = new Profession(0, '', '', '');
   public newProfession = new Profession(0, '', '', '');
   public professionUpdate = new Profession(0, '', '', '');
+  public response = {
+    code: 0,
+    message: ''
+  };
 
   constructor(private professionsMedecinsService: ProfessionsMedecinsService, private messageService: MessageService,
     private authenticationService: AuthenticationService, private confirmationService: ConfirmationService) {
@@ -56,6 +62,87 @@ export class ProfessionsMedecinsComponent implements OnInit {
 
   isAdmin() {
 
+  }
+
+  exportExcelProfessionsMedecins() {
+    console.log('Export excel file called: -> Professions');
+    this.professionsMedecinsService.exportProfessionsMedecins(this.authenticationService.getUsername()).subscribe(
+      data => {
+        console.log('Response: ' + JSON.stringify(data));
+        this.response = {
+          code: data.status,
+          message: data.statusText
+        };
+        if (this.response.message !== 'OK') {
+          this.messageService.add({
+            sticky: true,
+            severity: 'error',
+            summary: 'Erreur code ' + this.response.code,
+            detail: 'Message ' + this.response.message
+          });
+        } else {
+          this.downloadProfessionsFile(data);
+        }
+      },
+      error => {
+        this.messageService.add({
+          sticky: true,
+          severity: 'error',
+          summary: 'Erreur',
+          detail: 'Erreur Technique'
+        });
+      }
+    );
+  }
+
+  downloadProfessionsFile(data: any) {
+    const blob = new Blob([data.blob()], { type: 'application/octet-stream' });
+    saveAs(blob, 'Professions_OphthaCare.xlsx');
+  }
+
+  createProfession() {
+    this.blocked = true;
+    setTimeout(() => {
+      this.blocked = false;
+      this.displayNewDialog = true;
+    }, 500);
+  }
+
+  submitProfession(profession: Profession) {
+    console.log('Profession to register: ' + profession.nomProfession);
+    this.newProfession = profession;
+    this.blocked = true;
+    this.professionsMedecinsService.insertProfessionMedecin(this.newProfession, 'this.authenticationService.getUsername()').subscribe(
+      response => {
+        this.blocked = false;
+        if (response.json().code !== 'OK') {
+          this.messageService.add({
+            sticky: true,
+            severity: 'error',
+            summary: response.json().code,
+            detail: response.json().message
+          });
+        } else {
+          this.messageService.add({
+            sticky: false,
+            severity: 'success',
+            summary: 'Succès',
+            detail: 'Profession enregistré.'
+          });
+          this.getProfessions();
+          this.newProfession = new Profession(0, '', '', '');
+          this.displayNewDialog = false;
+        }
+      },
+      error => {
+        this.blocked = false;
+        this.messageService.add({
+          sticky: true,
+          severity: 'error',
+          summary: 'Erreur',
+          detail: 'Erreur Technique'
+        });
+      });
   }
 
   clearFilter(dt) {
