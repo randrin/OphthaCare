1984922712664a596131d25af484dee379c54d69
Randrin <nzeukang.randrin@accenture.com>
2019-09-17 21:33:31 +0200

    Update profile user backend

--- ophtacare/src/app/services/profilService.ts
+++ ophtacare/src/app/services/profilService.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http, ResponseContentType, Headers } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfilService {
 
-  constructor() { }
+  private downloadProfilelUrl = window['baseUrl'] + '/profile/download';
+  private uploadProfilelUrl = window['baseUrl'] + '/profile/upload';
+
+  constructor(public router: Router, private http: Http) {}
+
+  uploadProfil (file, caller) {
+    const uploadData = new FormData();
+    uploadData.append('profileImage', file, file.name);
+    const options = {
+      headers: new Headers({
+        'caller': caller
+      }),
+    };
+    return this.http.post(this.uploadProfilelUrl, uploadData, options);
+  }
 }
